9d9f56ed8799d170ae0bb302e1020deb590bf006
Heather Cox <heathermcx1@gmail.com>
2024-06-08 12:46:04 +1000

    turn ev charger on

--- evScript.js
+++ evScript.js
@@ -1,12 +1,17 @@
 const ewelink = require('ewelink-api')
 const config = require('./config.json')
 
-async function program() {
-    const connection = new ewelink(config.ewelink)
-
+async function TurnEvChargerOn() {
+    const connection = new ewelink(config.ewelink.loginInfo)
+    console.log(config.ewelink.evChargerId)
     const devices = await connection.getDevices()
-
     console.log(devices)
+    const evState = await connection.getDevicePowerState(config.ewelink.evChargerId)
+
+    console.log(evState)
+
+    const status = await connection.setDevicePowerState(config.ewelink.evChargerId, 'on');
+    console.log(status);
 }
 
-program()
+TurnEvChargerOn()
