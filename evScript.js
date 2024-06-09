d9bf6f58d412e9797a556b45ad5c704752956e83
Heather Cox <heathermcx1@gmail.com>
2024-06-09 11:49:37 +1000

    get amber price

--- evScript.js
+++ evScript.js
@@ -1,5 +1,6 @@
 const ewelink = require('ewelink-api')
 const config = require('./config.json')
+const amberUrl = "https://api.amber.com.au/v1"
 
 async function TurnEvChargerOn() {
     const connection = new ewelink(config.ewelink.loginInfo)
@@ -14,4 +15,40 @@ async function TurnEvChargerOn() {
     console.log(status);
 }
 
-TurnEvChargerOn()
+// TurnEvChargerOn()
+
+const getJSON = (path, options) => fetch(path, options).then((res) => res.json());
+
+function makeAPIRequest(url, path, options) {
+    return getJSON(`${url}/${path}`, options);
+  }
+
+//   return this.makeAPIRequest('admin/quiz/new', {
+//     method: 'POST',
+//     headers: {
+//       Authorization: `Bearer ${getLoggedInUserToken()}`,
+//       accept: 'application/json',
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       name: quizName,
+//     }),
+//   });
+
+async function GetCurrentAmberPrice() {
+    const result = await makeAPIRequest(
+        amberUrl,
+        `sites/${config.amber.siteId}/prices/current?resolution=30`,
+        {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${config.amber.apiToken}`,
+                accept: 'application/json',
+            }
+        }
+    )
+    console.log(result)
+    return result
+}
+
+GetCurrentAmberPrice()
